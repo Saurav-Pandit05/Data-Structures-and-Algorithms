86dd1a869c4ae7d1d81fe23426592ec1f1d04b35
Saurav Pandit <sauravpandit9691@gmail.com>
2025-07-11 21:11:12 +0530

    Add Patt1.java file

---
+++ Pattens/Patt1.java
@@ -0,0 +1,9 @@
+// Print Square Pattern 
+
+public class Patt1 {
+    public static void main(String[] args) {
+        for (int i=1; i<5; i++){
+            System.out.println("*****");
+        }
+    }
+}
